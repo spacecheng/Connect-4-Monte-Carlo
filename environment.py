7aa0f8bf419936b052d655c56853c2f299e0de43
randomologist <randomologist2000@gmail.com>
2026-04-02 21:01:25 -0500

    added diagonals and optimized horizontals

--- environment.py
+++ environment.py
@@ -39,6 +39,8 @@ class connect_4_game:
         width = self.cols
         max_right = width - mv_col
         max_left = mv_col + 1
+        max_up =  self.rows - mv_ht
+        max_down = mv_ht + 1
         #vertical check
         if mv_ht >= win_cond:
             for i in range(1,win_cond):
@@ -52,17 +54,56 @@ class connect_4_game:
         for skew in range(1,max_right): #right check
             if board[mv_idx + skew] == mv_mark:
                 aligned +=1
-                if aligned >= win_cond:
-                    return True
-            else:
+            else: #no match
                 break
+        if aligned >= win_cond: #win found
+            return True
         for skew in range(1,max_left):
             if board[mv_idx - skew] == mv_mark:
                 aligned +=1
+                if aligned >= win_cond: #win found before end of loop
+                    return True
+            else: #no match
+                break
+        #diagonal checks
+        aligned = 1
+        #up-right check
+        ur_lim = max_up if max_up < max_right else max_right
+        for skew in range(1,ur_lim):
+            if board[mv_idx + (skew * (width+1))] == mv_mark:
+                aligned +=1
+            else: #no match
+                break
+        #down-left check
+        dl_lim = max_down if max_down < max_left else max_left
+        for skew in range(1,dl_lim):
+            if board[mv_idx - (skew * (width+1))] == mv_mark:
+                aligned +=1
+                if aligned >= win_cond: #win found before end of loop
+                    return True
+            else: #no match
+                break
+        aligned = 1
+        #up-left check
+        ul_lim = max_up if max_up < max_left else max_left
+        for skew in range(1,ul_lim):
+            if board[mv_idx + (skew *width -1)] == mv_mark:
+                aligned +=1
+            else: # no match
+                break
+        if aligned >= win_cond:
+            return True
+        dr_lim = max_down if max_down < max_right else max_right
+        for skew in range(1,dr_lim):
+            if board[mv_idx - (skew * width -1)] ==mv_mark:
+                aligned +=1
                 if aligned >= win_cond:
                     return True
-            else:
+            else: #no match
                 break
+
+
+        
 
         
                         
