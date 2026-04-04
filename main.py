573e86be01bfccb8f26f9dc3c285dcd3fd4186bc
randomologist <randomologist2000@gmail.com>
2026-04-03 19:32:37 -0500

    intro text

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ to actualy train and test the bot
 '''
 import bot
 import environment
-game = environment.connect_4_game()
-state = game.init_state()
-game.display_board(state[0])
+def setup():
+    game = environment.connect_4_game()
+    state = game.init_state()
+    game.display_board(state[0])
+
+def play(pvp = False): #default play is against bot
+    setup()
+    if pvp: #local vs
+        pass
+    else: #play against bot
+        pass
+
+print("Welcome to our Connect 4 system")
+choice_1 = int(input("Would you like to play against a bot(1) or a nearby friend(2)"))
+if choice_1 == 1:
+    mcts = bot.placeholder()
+    choice_2 = int(input("Would you like to make the first move(1) or let the bot do so(2)?"))
+    if choice_2 == 1:
+        play()
+    else:
+        play(False,True)
