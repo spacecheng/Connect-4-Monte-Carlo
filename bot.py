1a76c04693c1c1e4fb614425095610cf19406507
randomologist <randomologist2000@gmail.com>
2026-04-04 19:30:56 -0500

    random bot with win check

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ tables and choosing action will be defined
 '''
 import environment
 import random
-from environment import help_check_win
+from environment import help_check_win as check_win
 
 class Node:
     def __init__(self, state, parent = None, move = None ):
@@ -17,8 +17,37 @@ class Node:
 
 class rand_bot:
     name = "random"
-    def act(board,heights,moveset):
+    def __init__(self,game):
+        self.game = game
+    def act(self,state,heights,moveset):
         r = random.random
+        move = moveset[int(r() * len(moveset))]
+        return move
+    
+class smart_rand:
+    name = "random+check"
+    def __init__(self,game):
+        self.game = game
+    def act(self,state,heights,moveset):
+        board, _, move_idx, _ = state
+        win_cond = self.game.win_cond
+        h = self.game.rows
+        w = self.game.cols
+        r = random.random
+        for move in moveset:
+            print(f"checking {move}")
+            new_height =heights[move]
+            move_idx = new_height*w + move
+            print(f"mvidx = {move_idx}")
+            res = check_win(win_cond,w,h,board,moveset,1,move,move_idx,new_height)
+            print(f"result{res}")
+            if res == 1:
+                print("found win")
+                return move
+            res = check_win(win_cond,w,h,board,moveset,-1,move,move_idx,new_height)
+            if res == 1:
+                print("found loss")
+                return move
         move = moveset[int(r() * len(moveset))]
         return move
     
